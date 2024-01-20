bd4011a3102c1a55c532953d2b0dbce1de850d9d
Toby Crisford <tobycrisford@gmail.com>
2024-01-20 14:38:56 +0000

    Adding fn to perform time step using RK4

--- gym_sym.py
+++ gym_sym.py
@@ -20,8 +20,13 @@ def diff_matrix(N):
     return D, x
 
 
-def compute_acceleration(D, D2, x, x_dot, boundary_val, dx, d2x, dx_dot, g):
+def compute_acceleration(D, D2, x, x_dot, boundary_val, g):
 
+    # Compute required derivatives
+    dx = D @ x
+    d2x = D2 @ x
+    dx_dot = D @ x_dot
+    
     # Prepare tension eqn
     tension_lhs = D2 - np.diag(np.sum(d2x**2,axis=1))
     tension_rhs = -1 * np.sum(dx_dot * d2x,axis=1)
@@ -52,14 +57,44 @@ def test_acceleration():
     x = np.column_stack((np.zeros(len(s)), -1 * s))
     x_dot = np.zeros(x.shape)
     boundary_val = np.zeros(2)
-    dx = D @ x
-    d2x = D2 @ x
-    dx_dot = D @ x_dot
     g = np.array([0,-1.0])
 
-    acceleration, tension = compute_acceleration(D, D2, x, x_dot, boundary_val, dx, d2x, dx_dot, g)
+    acceleration, tension = compute_acceleration(D, D2, x, x_dot, boundary_val, g)
 
     print(acceleration)
     print(tension)
 
     assert np.all(acceleration**2 < 10**(-5))
+
+
+def time_step_jump(D, D2, g, x, x_dot, t, boundary_fn, step_size):
+    '''Jump a time step using RK4'''
+
+    k1x = x_dot
+    k1xdot = compute_acceleration(D, D2, x, x_dot, boundary_fn(t), g)
+    
+    k2x = x_dot + 0.5 * step_size * k1xdot
+    k2xdot = compute_acceleration(D, D2,
+                                  x + 0.5 * step_size * k1x,
+                                  x_dot + 0.5 * step_size * k1xdot,
+                                  boundary_fn(t + 0.5 * step_size),
+                                  g)
+
+    k3x = x_dot + 0.5 * step_size * k2xdot
+    k3xdot = compute_acceleration(D, D2,
+                                  x + 0.5 * step_size * k2x,
+                                  x_dot + 0.5 * step_size * k2xdot,
+                                  boundary_fn(t + 0.5 * step_size),
+                                  g)
+
+    k4x = x_dot + step_size * k3xdot
+    k4xdot = compute_acceleration(D, D2,
+                                  x + step_size * k3x,
+                                  x_dot + step_size * k3xdot,
+                                  boundary_fn(t + step_size),
+                                  g)
+
+    new_x = x + (1/6) * step_size * (k1x + 2 * k2x + 2 * k3x + k4x)
+    new_xdot = x_dot + (1/6) * step_size * (k1xdot + 2 * k2xdot + 2 * k3xdot + k4xdot)
+
+    return new_x, new_xdot
