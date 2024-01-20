Creating test for acceleration computation and fixing uncovered typos

gym_sym.py
@@ -29,7 +29,7 @@ def compute_acceleration(D, D2, x, x_dot, boundary_val, dx, d2x, dx_dot, g):
     # Prepare free end tension boundary condition
     tension_lhs[0,:] = np.zeros(len(tension_lhs))
     tension_lhs[0,0] = 1.0
-    tenshion_rhs[0] = 0.0
+    tension_rhs[0] = 0.0
 
     # Prepare fixed end boundary condition
     tension_lhs[-1,:] = D[-1,:]
@@ -39,7 +39,27 @@ def compute_acceleration(D, D2, x, x_dot, boundary_val, dx, d2x, dx_dot, g):
     tension = solve(tension_lhs, tension_rhs)
 
     # Compute acceleration (imposing boundary value)
-    acceleration = (D @ tension) * dx + tension * d2x + np.outer(g * np.ones(len(D)))
+    acceleration = (D @ tension).reshape((len(tension),1)) * dx + tension.reshape((len(tension),1)) * d2x + np.outer(np.ones(len(D)),g)
     acceleration[-1,:] = boundary_val
 
-    return acceleration
+    return acceleration, tension
+
+
+def test_acceleration():
+
+    D, s = diff_matrix(10)
+    D2 = D @ D
+    x = np.column_stack((np.zeros(len(s)), -1 * s))
+    x_dot = np.zeros(x.shape)
+    boundary_val = np.zeros(2)
+    dx = D @ x
+    d2x = D2 @ x
+    dx_dot = D @ x_dot
+    g = np.array([0,-1.0])
+
+    acceleration, tension = compute_acceleration(D, D2, x, x_dot, boundary_val, dx, d2x, dx_dot, g)
+
+    print(acceleration)
+    print(tension)
+
+    assert np.all(acceleration**2 < 10**(-5))